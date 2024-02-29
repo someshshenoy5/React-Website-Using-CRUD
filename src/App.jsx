393234597a17d16a796ca4f807c9ec27f9608739
someshshenoy5 <shenoysomesh5@gmail.com>
2024-02-29 21:54:36 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Table from './Components/Table'
+
+const App = () => {
+
+
+  return (
+    <>
+    <Table/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Table.jsx
@@ -0,0 +1,101 @@
+import axios, { AxiosHeaders } from "axios";
+import React, { useEffect, useState } from "react";
+const Table = () => {
+  const [data, setData] = useState([]);
+  const [name,setName] = useState("")
+  const [email,setEmail] = useState("")
+  const [phone,setPhone] = useState("")
+  const [uname,usetName] = useState("")
+  const [uemail,usetEmail] = useState("")
+  const [uphone,usetPhone] = useState("")
+  const [editId,setEditId] = useState(-1)
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/users")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id =data[data.length-1] .id +1;
+    axios.post("http://localhost:3000/users",{id: id , name: name, email: email, phone: phone})
+    .then((res) => location.reload())
+    .catch((err)=>console.log(er))
+}
+const handleEdit = (id) => {
+    axios
+      .get(`http://localhost:3000/users/${id}`)
+      .then((res) => {usetName(res.data[0].name) 
+        usetEmail(res.data[0].email) 
+        usetPhone(res.data[0].phone)})
+      .catch((err) => console.log(err));
+  setEditId(id)
+    
+}
+const handleUpdate=() =>{
+  axios.put(`http://localhost:3000/users/${editId}`,{id: editId , name: uname, email: uemail, phone: uphone})
+   .then((res)=>{location.reload()
+setEditId(-1)})
+.catch(err=>console.log(err))
+}
+const handleDelete =(id)=>{
+    axios.delete(`http://localhost:3000/users/${id}`)
+ .then(res => {location.reload()})
+ .catch(err=>console.log(err))
+}
+  return (
+    <div className="container">
+      <div className="form-div">
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Enter Your Name"  onChange={e => setName(e.target.value)}/><br />
+            <input type="text" placeholder="Enter Your Email" onChange={e => setEmail(e.target.value)}/><br/>
+            <input type="text" placeholder="Enter Your Phone" onChange={e => setPhone(e.target.value)}/><br/>
+            <button type="submit">Add </button>
+          </form>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((user, index) => (
+                
+                  user.id === editId ? 
+                <tr>
+                    <td>{user.id}</td>
+                   <td> <input type="text"  value={uname} onChange={e => usetName(e.target.value)}/></td>
+                   <td>
+                     <input type='email' value={uemail} onChange={e => usetEmail(e.target.value)}/>
+                   </td>
+                   <td>
+                     <input type="Number" value={uphone} onChange={e => usetPhone(e.target.value)}/>
+                    </td>
+                    <td><button onClick={handleUpdate}>Update</button></td>
+                </tr> :
+                <tr key={index}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+                <td>
+                  <button onClick={()=> handleEdit(user.id)}>Edit</button>
+                  <button onClick={()=>handleDelete(user.id)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Table;
